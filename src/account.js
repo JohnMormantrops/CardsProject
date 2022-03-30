0e6870895101150de2bf1c83d3dfd8546bdedb32
JohnMormantrops <90422980+JohnMormantrops@users.noreply.github.com>
2022-03-30 12:52:14 +0100

    Delete src directory

--- src/account.js
+++
@@ -1,7 +0,0 @@
-export default function () {
-  return (
-    <div className="acc">
-      <h1>HERES YPUIR AACCCCCOIUJMT</h1>
-    </div>
-  );
-}
--- src/home.js
+++
@@ -1,9 +0,0 @@
-function Home() {
-  return (
-    <div className="home">
-      <h1>Hopmepage</h1>
-    </div>
-  );
-}
-
-export default Home;
