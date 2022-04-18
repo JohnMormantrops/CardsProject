904b67a205f5910a8ccd1720b79983933f18f3a5
lcroker <luke.croker.2022@mumail.ie>
2022-04-18 18:16:48 +0100

    getCards page

--- src/getCards.js
+++ src/getCards.js
@@ -1,31 +1,37 @@
 import React from "react";
-import  {useState, useEffect} from "react";
-import {apiKey} from "./apiKey";
+import { useState, useEffect } from "react";
+import { apiKey } from "./apiKey";
 
+function Cards() {
+  const [allCards, setAllCards] = useState([]);
 
-function Cards(){
+  useEffect(async () => {
+    const headers = {
+      // "Access-Control-Allow-Origin": '*',
+      "content-type": "application/json",
+      jwtToken: apiKey,
+    };
+    const apiResponse = await fetch("http://localhost:3000/card/", { headers });
+    apiResponse.json().then((data) => {
+      setAllCards(data);
+    });
+  }, []);
 
-    const [allCards, setAllCards] = useState([])    
-    
-    useEffect(async() => {
+  console.log("HERES THE CARD DATA FOR ALL CARDS");
+  console.log(allCards);
 
-        const headers = {
-            // "Access-Control-Allow-Origin": '*',
-            "content-type" : "application/json",
-            "jwtToken": apiKey
-            
-        }
-        const apiResponse = await fetch("http://localhost:3000/card/", { headers })
-        apiResponse.json().then(data => {
-            setAllCards(data);
-        });
-    }, [])
-    
-    console.log("HERES THE CARD DATA FOR ALL CARDS")       
-    console.log(allCards)
+  return (
+    <div>
+      <p id="getCardsPara">
+        <h3>CARDS</h3>
+        {allCards.map((card, key) => (
+          <div>
+            {card.name} {card.nation} {card.paragraph} check
+          </div>
+        ))}
+      </p>
+    </div>
+  );
+}
 
-    return (<div>CARDS<p>
-        {allCards.map((card, key) =>(<div>{card.name} {card.nation} {card.paragraph} check</div> ))}</p></div>);
-};
-
-export default Cards
+export default Cards;
