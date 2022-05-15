66b6bd94aa52427abc2e57c69edecfbade28ddc2
John Gorman <gormanjohn2009@gmail.com>
2022-05-15 23:52:10 +0100

    stuff

--- src/home.js
+++ src/home.js
@@ -10,8 +10,23 @@ function Home() {
   return (
     <div className="home">
       <div className="title">
-        <br></br><br></br><br></br><br></br>
-        <div>{Logo()}</div>
+        {/* <br></br><br></br><br></br><br></br> */}
+
+        <div className="row">
+
+
+          <div>
+          <p className="welcome">
+            <h1>World of Worship!!</h1>
+            All over the world, and throughout history,
+            people have always found something to worship.
+            Who will you choose? Whose gods are the strongest?
+            Get started and choose your gods now.<br/>
+          <button className="getCards">GET CARDS</button>
+           </p></div>
+
+        <div >{Logo()}</div>
+        </div>
       </div>
     </div>
   );
